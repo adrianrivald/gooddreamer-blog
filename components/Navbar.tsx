c4b9c63b4f6281704f0e5c41fec1f86fe5c91f7e
Adrian Rivaldy <adrianrivaldy@gmail.com>
2024-05-14 19:30:36 +0700

    hotfix: logo click action

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import React from "react";
 
 export const Navbar = () => {
+  const router = useRouter();
+  const onClickLogo = () => {
+    router.push("/");
+  };
   return (
     <div className="max-w-[120rem] h-[60px] mx-auto flex justify-between items-center py-8 px-20 w-full fixed z-50 bg-white">
       <Image
@@ -9,6 +14,8 @@ export const Navbar = () => {
         width={200}
         height={27}
         alt="gooddreamer-logo"
+        onClick={onClickLogo}
+        className="cursor-pointer"
       />
       <ol className="flex gap-12 justify-between text-purple-primary">
         <li className="cursor-pointer">Pengumuman</li>
