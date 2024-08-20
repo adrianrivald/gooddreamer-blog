2a39dfec922449bc2ae17b3319df92fe0d492d23
Adrian Rivaldy <adrianrivaldy@gmail.com>
2024-08-20 15:50:45 +0700

    fix bug hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,8 +13,8 @@ export const Hero = () => {
   const banners = data?.data;
   console.log(data, "data");
 
-  const onToDetail = (uri: string) => {
-    router.push(`${uri}`);
+  const onToWebsite = () => {
+    router.push("https://gooddreamer.id");
   };
 
   const settings = {
@@ -33,24 +33,54 @@ export const Hero = () => {
   console.log(banners, "banners");
 
   return (
-    <Slider className="overflow-hidden" {...settings}>
-      {banners &&
-        banners?.slice(0, 6).map((banner: any) => (
-          <Link
-            href={banner?.url}
-            target="_blank"
-            className="mt-2 text-base cursor-pointer w-full focus:outline-none"
-            key={banner?.url}
-          >
-            <img
-              src={banner?.image}
-              width={1920}
-              height={600}
-              className="object-cover w-full max-h-[600px] max-w-[1920px] m-auto"
-              alt="banner"
-            />
-          </Link>
-        ))}
-    </Slider>
+    <>
+      {banners?.length ? (
+        <Slider className="overflow-hidden" {...settings}>
+          {banners &&
+            banners?.slice(0, 6).map((banner: any) => (
+              <Link
+                href={banner?.url}
+                target="_blank"
+                className="mt-2 text-base cursor-pointer w-full focus:outline-none"
+                key={banner?.url}
+              >
+                <img
+                  src={banner?.image}
+                  width={1920}
+                  height={600}
+                  className="object-cover w-full max-h-[600px] max-w-[1920px] m-auto"
+                  alt="banner"
+                />
+              </Link>
+            ))}
+        </Slider>
+      ) : (
+        <div className="w-full relative bg-gradient-to-r from-[#D3CAE1] to-white min-h-[520px]">
+          <div className="absolute top-[16%] left-[5%] text-[20px] lg:text-[50px] text-purple-primary font-bold">
+            <div>
+              <div className="bg-white py-1 px-2 inline-block">
+                <h2>Menembus Batas Imajinasi</h2>
+              </div>
+            </div>
+            <div className="mt-4">
+              <div className="bg-white py-1 px-2 inline-block">
+                <h2>Jelajahi Dunia-dunia Baru</h2>
+              </div>
+            </div>
+            <div className="mt-4">
+              <div className="bg-white py-1 px-2 inline-block">
+                <h2>di Website Novel Kami!</h2>
+              </div>
+            </div>
+            <button
+              onClick={onToWebsite}
+              className="mt-4 lg:mt-0 bg-yellow-primary p-[10px] rounded-[8px] text-purple-primary text-[14px] w-[200px]"
+            >
+              Jelajahi Sekarang
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
