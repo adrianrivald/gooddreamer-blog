08861939e8d7cc4f0d45cbdc8dfa17255e096f4c
adrianrivald <adrianrivaldy@gmail.com>
2024-11-15 13:20:50 +0700

    fix gsp

--- components/FAQ-GSP.tsx
+++ components/FAQ-GSP.tsx
@@ -8,6 +8,7 @@ export const FaqGsp = () => {
   const { data, isLoading } = useFaqHomeList();
   const [toggleFaq1, setToggleFaq1] = React.useState(false);
   const [toggleFaq2, setToggleFaq2] = React.useState(false);
+  const [toggleFaq3, setToggleFaq3] = React.useState(false);
   const faqData = data?.data;
 
   const onToggleFaq1 = () => {
@@ -18,6 +19,12 @@ export const FaqGsp = () => {
 
   const onToggleFaq2 = () => {
     setToggleFaq2((prev) => {
+      return !prev;
+    });
+  };
+
+  const onToggleFaq3 = () => {
+    setToggleFaq3((prev) => {
       return !prev;
     });
   };
@@ -33,7 +40,7 @@ export const FaqGsp = () => {
           onClick={onToggleFaq1}
           className="cursor-pointer flex justify-between text-[20px]"
         >
-          <span>{faqData && faqData[0]?.question}</span>
+          <span>Apakah ada target pendapatan setiap bulannya?</span>
           <Image
             src="/blog/assets/icons/chevron-up.svg"
             width={14}
@@ -54,7 +61,11 @@ export const FaqGsp = () => {
               toggleFaq1 ? "block" : "hidden"
             } transition-all`}
           >
-            <p>{faqData && faqData[0]?.answer}</p>
+            <p>
+              Tidak ada target. Semakin rajin Storyteller membagikan
+              tautan/link, maka semakin besar juga potensi komisi yang akan
+              didapatkan.
+            </p>
           </div>
         </div>
       </div>
@@ -63,7 +74,7 @@ export const FaqGsp = () => {
           onClick={onToggleFaq2}
           className="cursor-pointer flex justify-between text-[20px]"
         >
-          <span>{faqData && faqData[1]?.question}</span>
+          <span>Apakah ada target posting konten?</span>
           <Image
             src="/blog/assets/icons/chevron-up.svg"
             width={14}
@@ -84,7 +95,45 @@ export const FaqGsp = () => {
               toggleFaq2 ? "block" : "hidden"
             } transition-all`}
           >
-            <p>{faqData && faqData[1]?.answer}</p>
+            <p>
+              Tidak ada ketentuan jumlah posting konten tiap bulannya.
+              Storyteller berhak menentukan keaktifannya sendiri.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={`transition-all border-b border-grey-third pb-4  `}>
+        <div
+          onClick={onToggleFaq3}
+          className="cursor-pointer flex justify-between text-[20px]"
+        >
+          <span>
+            Apakah konten untuk promosi harus melalui proses approval?
+          </span>
+          <Image
+            src="/blog/assets/icons/chevron-up.svg"
+            width={14}
+            height={14}
+            alt="chevron"
+            className={`${
+              toggleFaq3 ? "rotate-0" : "rotate-180"
+            }  transition-all`}
+          />
+        </div>
+        <div
+          className={`transition-all ${
+            toggleFaq3 ? "min-h-[100px]" : "min-h-[0px]"
+          }`}
+        >
+          <div
+            className={`mt-4 bg-white p-4 rounded-[10px] text-[20px] overflow-hidden  ${
+              toggleFaq3 ? "block" : "hidden"
+            } transition-all`}
+          >
+            <p>
+              Tidak harus. Konten bisa dikreasikan dan disesuaikan dengan minat
+              followers/audiens masing-masing.
+            </p>
           </div>
         </div>
       </div>
