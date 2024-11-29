13ff8211635bf88f231d33b0efde3b71a5b7de6a
adrianrivald <adrianrivaldy@gmail.com>
2024-11-29 13:47:22 +0700

    fix faq

--- components/FAQ-GSP.tsx
+++ components/FAQ-GSP.tsx
@@ -8,7 +8,24 @@ export const FaqGsp = () => {
   const { data, isLoading } = useFaqHomeList();
   const [toggleFaq1, setToggleFaq1] = React.useState(false);
   const [toggleFaq2, setToggleFaq2] = React.useState(false);
-  const faqData = data?.data;
+  const [toggleFaq3, setToggleFaq3] = React.useState(false);
+  const faqData = [
+    {
+      question: "Apakah ada target pendapatan setiap bulannya?",
+      answer:
+        "Tidak ada target. Semakin rajin Storyteller membagikan tautan/link, maka semakin besar juga potensi komisi yang akan didapatkan.",
+    },
+    {
+      question: "Apakah ada target posting konten?",
+      answer:
+        "Tidak ada ketentuan jumlah posting konten tiap bulannya. Storyteller berhak menentukan keaktifannya sendiri.",
+    },
+    {
+      question: "Apakah konten untuk promosi harus melalui proses approval?",
+      answer:
+        "Tidak harus. Konten bisa dikreasikan dan disesuaikan dengan minat followers/audiens masing-masing.",
+    },
+  ];
 
   const onToggleFaq1 = () => {
     setToggleFaq1((prev) => {
@@ -18,6 +35,12 @@ export const FaqGsp = () => {
 
   const onToggleFaq2 = () => {
     setToggleFaq2((prev) => {
+      return !prev;
+    });
+  };
+
+  const onToggleFaq3 = () => {
+    setToggleFaq3((prev) => {
       return !prev;
     });
   };
@@ -85,6 +108,36 @@ export const FaqGsp = () => {
             } transition-all`}
           >
             <p>{faqData && faqData[1]?.answer}</p>
+          </div>
+        </div>
+      </div>
+      <div className={`transition-all border-b border-grey-third pb-4  `}>
+        <div
+          onClick={onToggleFaq3}
+          className="cursor-pointer flex justify-between text-[20px]"
+        >
+          <span>{faqData && faqData[2]?.question}</span>
+          <Image
+            src="/blog/assets/icons/chevron-up.svg"
+            width={14}
+            height={14}
+            alt="chevron"
+            className={`${
+              toggleFaq3 ? "rotate-0" : "rotate-180"
+            }  transition-all`}
+          />
+        </div>
+        <div
+          className={`transition-all ${
+            toggleFaq3 ? "min-h-[100px]" : "min-h-[0px]"
+          }`}
+        >
+          <div
+            className={`mt-4 bg-white p-4 rounded-[10px] text-[20px] overflow-hidden  ${
+              toggleFaq3 ? "block" : "hidden"
+            } transition-all`}
+          >
+            <p>{faqData && faqData[2]?.answer}</p>
           </div>
         </div>
       </div>
